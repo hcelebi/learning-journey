cfe7106024dac6c8e9c5eeae35ef3ebddef88013
hakan celebi <hcelebi@ebay.com>
2021-06-16 14:22:10 +0300

    update learning journey provider host name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const {LearningJourneyService} = require('./service/LearningJourneyService')
 
 app.get('/learning-journey', async (req, res) => {
-    const learningJourneyService = new LearningJourneyService('http://learning-journey-back-end:8080/');
+    const learningJourneyService = new LearningJourneyService('http://learning-journey-provider:8080/');
 
     res.send(await learningJourneyService.getMessage())
 });
